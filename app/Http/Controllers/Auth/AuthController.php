2a70367023f98bbcc137af0dd838364aca6d59f7
Rik Aikman <rikaikman@hotmail.com>
2018-04-04 17:37:06 +0200

    added rol to register

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -52,6 +52,7 @@ class AuthController extends Controller
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
+            'rol'=>'required',
         ]);
     }
 
@@ -66,6 +67,7 @@ class AuthController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'rol'=> $data['rol'],
             'password' => bcrypt($data['password']),
         ]);
     }
