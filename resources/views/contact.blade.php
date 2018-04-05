1656a36a488b1ec212639ed7cb8b0ac601d77123
Rik Aikman <rikaikman@hotmail.com>
2018-04-05 10:58:03 +0200

    presentatie

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,12 +1,14 @@
-<html>
-    <body>
+@extends('layouts.app')
+
+@section('content')
+
         <h1>Contact</h1><br>
 
         <a href="http://localhost:8000/">Home</a><br>
         <a href="http://localhost:8000/profiel">Profiel</a><br>
         <a href="http://localhost:8000/dashboard">Dashboard</a><br>
-        <a href="http://localhost:8000/nieuws">Nieuws</a><br>
+        <a href="http://localhost:8000/nieuwsposts">Nieuws</a><br>
         <a href="http://localhost:8000/community">Community</a><br>
         <a href="http://localhost:8000/resources">Resources</a><br>
-    </body>
-</html>
+
+@endsection
