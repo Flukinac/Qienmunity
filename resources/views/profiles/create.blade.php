68394d05463a0ff2a6b659043e29baad8c419dc8
alexanderlexis <alexander.lexis@gmail.com>
2018-04-09 13:44:38 +0200

    lekker mergen

--- resources/views/profiles/create.blade.php
+++ resources/views/profiles/create.blade.php
@@ -2,5 +2,12 @@
 
 @section('content')
         <h1>Nieuw profiel aanmaken</h1>
+        {!! Form::open(['action' => 'ProfileController@store', 'method' => 'POST']) !!}
+            <div class='form-group'>
+                
+
+            </div>
+
+        {!! Form::close() !!}
             
 @endsection
