be2880d37fad8f501eb4ef069d76a273589c5e93
Steven Visser <sevisser1@gmail.com>
2018-04-09 17:12:05 +0200

    mail doet het

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Mail;
+use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
+//use Illuminate\Support\Facades\Mail;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -12,16 +14,17 @@ use App\Http\Controllers\Controller;
 class ContactController extends Controller
 {
     public function sendContact(Request $request){
-        $mail = $request->json()->all();
-        $data = array('name'=>$mail['subject']);
+        $title = $request->json()->all()['subject'];
+        $content = $request->json()->all()['text'];
         
-        mail::send(['text'=>$mail['text']], $data ,function($message){
-            $message->to('sevisser1@gmail.com','aan mij')->subject('Laravel Basic Testing Mail');
+        mail::send('contact', ['title' => $title, 'content' => $content] ,function($message){
+            $message->to('sevisser1@gmail.com','aan mij');
             $message->from('sevisser1@gmail.com','mijzlef');
+            $message->subject('Laravel Basic Testing Mail');
         });
         echo "Basic Email Sent. Check your inbox.";
     }
 }
 
-       
+ 
  
