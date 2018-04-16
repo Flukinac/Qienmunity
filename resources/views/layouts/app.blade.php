baa945979f3e3175204af21dfdc02be660878b5c
Rik Aikman <rikaikman@hotmail.com>
2018-04-16 12:02:35 +0200

    authentication works

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    @unless (Auth::check() || Request::url() == "/login")
-        <script>window.location.href = "/login";</script>
-    @endunless
-   
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
