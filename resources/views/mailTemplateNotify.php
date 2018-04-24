8d4ef559e4910e04190842db1e051f99c2932e16
Steven Visser <sevisser1@gmail.com>
2018-04-24 14:04:08 +0200

    laatste versie

---
+++ resources/views/mailTemplateNotify.php
@@ -0,0 +1,9 @@
+<html>
+<head></head>
+<body>
+    <p>{{$content}}</p><br>
+    <a href="http://localhost/unsign?mail={{$email['email']}}"><h3>Meld u af van deze automatische mailservice door op deze link te klikken</h3></a><br><br>
+    <div>afzender Qienmunity | {{$sendFrom}}</div><br>
+    
+</body>
+</html>
