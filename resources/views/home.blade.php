eeb507cfffcff118e00793685dde39837707b945
alexanderlexis <alexander.lexis@gmail.com>
2018-04-24 10:14:17 +0200

    steven mag pullen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,16 @@
 
 <div class="container">
     <div class="row">
+        
+        <!--Nieuwposts tonen-->
+        
                 <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading" id="qien--background-colour"><h2><a href='/nieuwsposts'>Nieuws</a></h2></div>
                     <div class="panel-body">
                         @foreach($nieuwspost as $post)
                             <div class='well'>
-                                <h3><a href="/communitypost/{{$post->id}}">{{$post->title}}</a></h3>
+                                <h3><a href="/nieuwsposts/{{$post->id}}">{{$post->title}}</a></h3>
                                 <p>{!!str_limit($post->content, 300)!!}</p>
                                 <small>Geschreven op {{$post->updated_at}} door <a href='/profiles/{{$post->user->profile->id}}'>{{$post->user->name}}</a></small>
                             </div>
@@ -22,6 +25,9 @@
                     </div> 
             </div>
         </div>
+        
+        <!--Communitypost tonen-->
+        
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading" id="qien--background-colour"><h2><a href='/communitypost'>Community</a></h2></div>
@@ -36,6 +42,14 @@
                     </div> 
             </div>
         </div>
+        
+        <!--Video-->
+        
+        <div class="col-md-10 col-md-offset-1">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/u9Mv98Gr5pY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        
+        
     </div>
 </div>
 
