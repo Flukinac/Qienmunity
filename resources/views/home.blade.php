e06310193b2accbc32e263324670bcd35fd8ba40
alexanderlexis <alexander.lexis@gmail.com>
2018-04-24 14:34:08 +0200

    dashboard gaat lekker

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,39 +3,44 @@
 @section('content')
 
 <h1>Dashboard</h1>
-<p>Welkom, {{auth()->user()->name}}</p>
+<h4>Welkom bij jouw Qienmunity, {{auth()->user()->name}}!</h4>
 
 
 <div class="container">
     <div class="row">
-        
         <!--Nieuwposts tonen-->
-            <div class="col-lg-6">          
+            <div class="col-lg-6">   
+                
                 <a href='/nieuwsposts'><h2>Nieuws</h2></a>
-                @foreach($nieuwspost as $post)
-                <div class="card">
-                    <div class="card-body">
-                        <div class='well'>
-                            <h3 class="card-title" ><a href="/nieuwsposts/{{$post->id}}">{{$post->title}}</a></h3>
-                            <p>{!!str_limit($post->content, 300)!!}</p>
-                            <small>Geschreven op {{$post->updated_at}} door <a href='/profiles/{{$post->user->profile->id}}'>{{$post->user->name}}</a></small>
-                        </div>
-                    </div>
-                </div>
-                @endforeach
+                       <div class="well" style="box-shadow: 10px 10px 5px #aaaaaa;">
+                            @foreach($nieuwspost as $post)
+                            <div>
+                                <div class='well'>
+                                    <h3 class="card-title" ><a href="/nieuwsposts/{{$post->id}}">{{$post->title}}</a></h3>
+                                    <p>{!!str_limit($post->content, 300)!!}</p>
+                                    <small>Geschreven op {{$post->updated_at}} door <a href='/profiles/{{$post->user->profile->id}}'>{{$post->user->name}}</a></small>
+                                </div>
+                            </div>
+                            @endforeach
+                            <a href="/nieuwsposts">Bekijk alle nieuwsposts ></a>
+                       </div>
+                
             </div>
         
                 <!--Toon 2 nieuwste gebruikers-->
         
             <div class="col-lg-6">
                 <a href='/profiles'><h2>Nieuwste gebruikers</h2></a>
-                @foreach($profiles as $profile)
-                    <div class='well'>
-                        <a href="/profiles/{{$profile->id}}"><img class="img-circle" width=50px src="{{ route('profile.image', ['filename' => $profile->username . '-' . $profile->user_id . '.jpg']) }}" alt="" class="img-responsive"></a>
-                        <h3><a href="/profiles/{{$profile->id}}">{{$profile->username}}</a></h3>
-                        <p>{!!str_limit($profile->biography, 300)!!}</p>
-                    </div>
-                @endforeach
+                <div class="well" style="box-shadow: 10px 10px 5px #aaaaaa;">
+                    @foreach($profiles as $profile)
+                        <div class='well'>
+                            <a href="/profiles/{{$profile->id}}"><img class="img-circle" width=50px src="{{ route('profile.image', ['filename' => $profile->username . '-' . $profile->user_id . '.jpg']) }}" alt="" class="img-responsive"></a>
+                            <h3><a href="/profiles/{{$profile->id}}">{{$profile->username}}</a></h3>
+                            <p>{!!str_limit($profile->biography, 300)!!}</p>
+                        </div>
+                    @endforeach
+                    <a href="/profiles">Bekijk alle profielen ></a>
+                </div>
             </div>
         
     </div>
@@ -45,6 +50,7 @@
         
         <div class="col-lg-6">
             <a href='/communitypost'><h2>Community</h2></a>
+              <div class="well" style="box-shadow: 10px 10px 5px #aaaaaa;">
             @foreach($commpost as $post)
                 <div class='well'>
                     <h3><a href="/communitypost/{{$post->id}}">{{$post->title}}</a></h3>
@@ -52,12 +58,19 @@
                     <small>Geschreven op {{$post->updated_at}} door <a href='/profiles/{{$post->user->profile->id}}'>{{$post->user->name}}</a></small>
                 </div>
             @endforeach
+            <a href="/communitypost">Bekijk alle communityposts ></a>
+              </div>
         </div>
         
         <!--Video-->
         
         <div class="col-lg-6">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/u9Mv98Gr5pY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <h2>Featured video</h2>
+            <div class="well">
+                <div class="videoWrapper">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/9cKsq14Kfsw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                </div>
+            </div>
         </div>
         
     </div>
