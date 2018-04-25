df0c23c6525c151ada9efb788197b219d5c63ff0
alexanderlexis <alexander.lexis@gmail.com>
2018-04-25 21:23:09 +0200

    klein beetje aangepast

--- resources/views/profiles/myprofile.blade.php
+++ resources/views/profiles/myprofile.blade.php
@@ -12,7 +12,7 @@
                 <div class="spacer" id="prof-spacer"></div>
                 <br>
                 @if (Storage::disk('local')->has($profile->username . '-' . $profile->user->id . '.jpg'))
-                <img class="img-circle profile-img" width="250px" src="{{ route('profile.image', ['filename' => $profile->username . '-' . $profile->user->id . '.jpg']) }}" alt="Profiel Foto">
+                <img class="img-circle profile-img" height='250px' width="250px" src="{{ route('profile.image', ['filename' => $profile->username . '-' . $profile->user->id . '.jpg']) }}" alt="Profiel Foto">
                 @endif
                 <div class="card-body">
                 <h5 class="card-title"><b>{{Auth::user()->profile->username}}</b></h5>
